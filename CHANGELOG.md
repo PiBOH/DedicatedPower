fbacabf8d26445cae6fc8017d23c2a7bb70fdf36
PiBOH <piboh.github@gmail.com>
2026-08-07 14:59:25 +0200

    v26.2-1.0.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [26.2-1.0.9] - 2026-08-07
+
+### Added
+- Added a `Done!` ASCII ready banner when the server finishes starting, working both with and without Geyser
+- Inspired by Jarock's `server-ready-banner.txt`: [PiBOH/jarock](https://github.com/PiBOH/jarock)
+
 ## [26.2-1.0.8] - 2026-08-07
 
 ### Fixed
--- src/main/java/net/supersirvu/DedicatedPower.java
+++ src/main/java/net/supersirvu/DedicatedPower.java
@@ -7,6 +7,7 @@
 package net.supersirvu;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,8 +17,21 @@ public class DedicatedPower implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	/*
+	 * Inspired by Jarock's server-ready-banner.txt:
+	 * https://github.com/PiBOH/jarock
+	 */
+	private static final String READY_BANNER = "                           ▄▄\n"
+			+ "████▄   ▄▄▄  ▄▄  ▄▄ ▄▄▄▄▄  ██\n"
+			+ "██  ██ ██▀██ ███▄██ ██▄▄   ██\n"
+			+ "████▀  ▀███▀ ██ ▀██ ██▄▄▄  ▄▄";
+
 	@Override
 	public void onInitialize() {
-
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+			// Queue the banner after all SERVER_STARTED callbacks have run. This keeps
+			// it after optional integrations such as Geyser finish their startup work.
+			server.execute(() -> LOGGER.info("\n{}\nThe DedicatedPower server has finished loading.\n", READY_BANNER));
+		});
 	}
 }
